20b9cc1bc21317a271d5212421d6e19d397f9ae0
servon <servonlewis@gmail.com>
2021-11-07 00:13:14 -0400

    continuing

--- lib/core/controller/firebase_user.dart
+++ lib/core/controller/firebase_user.dart
@@ -1,28 +1,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirebaseUser {
-  FirebaseUser(
-      {required this.age,
-      this.avatar,
-      this.experience,
-      required this.first_name,
-      required this.last_name,
-      this.gender,
-      required this.height,
-      required this.weight,
-      this.goals,
-      this.injuries,
-      required this.email});
+  FirebaseUser({
+    this.age,
+    this.avatar,
+    this.experience,
+    this.first_name,
+    this.last_name,
+    this.gender,
+    this.height,
+    this.weight,
+    this.goals,
+    this.injuries,
+    required this.email,
+  });
 
-  final int age;
+  final int? age;
   final String? avatar;
   final String email;
   final String? experience;
-  final String first_name;
-  final String last_name;
+  final String? first_name;
+  final String? last_name;
   final String? gender;
-  final String height;
-  final String weight;
+  final String? height;
+  final String? weight;
   final List<String>? goals;
   final List<String>? injuries;
 
@@ -32,11 +33,11 @@ class FirebaseUser {
           avatar: json['avatar']! as String?,
           email: json['email']! as String,
           experience: json['experience']! as String?,
-          first_name: json['first_name']! as String,
-          last_name: json['last_name']! as String,
+          first_name: json['first_name']! as String?,
+          last_name: json['last_name']! as String?,
           gender: json['gender']! as String?,
-          height: json['height']! as String,
-          weight: json['weight']! as String,
+          height: json['height']! as String?,
+          weight: json['weight']! as String?,
           goals: json['goals']! as List<String>?,
           injuries: json['injuries']! as List<String>?,
         );
@@ -56,8 +57,18 @@ class FirebaseUser {
     };
   }
 
+  /// Get a reference to the single user.
   DocumentReference<FirebaseUser> getDocumentReference() {
     return FirebaseFirestore.instance.collection('users').doc(email).withConverter<FirebaseUser>(
+          fromFirestore: (snapshot, _) => FirebaseUser.fromJson(snapshot.data()!),
+          toFirestore: (movie, _) => movie.toJson(),
+        );
+  }
+
+  /// Get a reference to the entire collection.
+  /// Dont forget to add a where clause.
+  CollectionReference<FirebaseUser> getCollectionReference() {
+    return FirebaseFirestore.instance.collection('users').withConverter<FirebaseUser>(
           fromFirestore: (snapshot, _) => FirebaseUser.fromJson(snapshot.data()!),
           toFirestore: (movie, _) => movie.toJson(),
         );
