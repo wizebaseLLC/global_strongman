1d8dbd9267df3823b3747d0907d85879c5ccc0b2
servon <servonlewis@gmail.com>
2022-01-10 17:24:42 -0500

    first version done, need to remove shrinkwraps though

--- lib/widget_tree/profile_screen/view/secondary_screens/progress_gallery/progress_gallery.dart
+++ lib/widget_tree/profile_screen/view/secondary_screens/progress_gallery/progress_gallery.dart
@@ -1,18 +1,9 @@
-import 'dart:io';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
-import 'package:global_strongman/constants.dart';
-import 'package:global_strongman/core/model/ProgressGalleryCard.dart';
 import 'package:global_strongman/core/model/firebase_user.dart';
 import 'package:global_strongman/core/view/platform_scaffold_ios_sliver_title.dart';
-import 'package:global_strongman/widget_tree/profile_screen/view/secondary_screens/progress_gallery/gallery_image_card.dart';
 import 'package:global_strongman/widget_tree/profile_screen/view/secondary_screens/progress_gallery/gallery_stream.dart';
-import 'package:global_strongman/widget_tree/profile_screen/view/secondary_screens/progress_gallery/line_graph.dart';
 import 'package:global_strongman/widget_tree/profile_screen/view/secondary_screens/progress_gallery/public_switch.dart';
-import 'package:global_strongman/widget_tree/profile_screen/view/secondary_screens/progress_gallery/share_or_add_button.dart';
 
 class ProgressGallery extends StatelessWidget {
   const ProgressGallery({
